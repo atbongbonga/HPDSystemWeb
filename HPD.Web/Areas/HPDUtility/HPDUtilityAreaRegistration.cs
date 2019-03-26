1b035256cce940ffa40962eb727a7ac5750c8ccd
atbongbonga <alvenbongbonga03@gmail.com>
2019-03-26 11:23:33 +0800

    Set up Area

---
+++ HPD.Web/Areas/HPDUtility/HPDUtilityAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace HPD.Web.Areas.HPDUtility
+{
+    public class HPDUtilityAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "HPDUtility";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "HPDUtility_default",
+                "HPDUtility/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
