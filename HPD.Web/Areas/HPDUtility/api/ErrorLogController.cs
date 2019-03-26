33e930b4882a58f430eac43ee250e22ca9fa6db6
atbongbonga <alvenbongbonga03@gmail.com>
2019-03-26 16:50:45 +0800

    Create API for creating ErrorLog

---
+++ HPD.Web/Areas/HPDUtility/api/ErrorLogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DAL.Logics;
+
+namespace HPD.Web.Areas.HPDUtility.api
+{
+    public class ErrorLogController : ApiController
+    {
+
+        [HttpPost]
+        public bool CreateErrorMessage(string empcode, string errmsg, string ipadd, string empid, string program, DateTime createddate, string type, string errpic)
+        {
+            var errorLogComponent = new ErrorLogComponent();
+            var result = errorLogComponent.CreateErroLog(empcode, errmsg, ipadd, empid, program, createddate, type, errpic);
+            return true;
+        }
+    }
+}
--- HPD.Web/obj/Debug/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HPD.Web.MvcApplication" Language="C#" %>
